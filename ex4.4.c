3b1ab6194b38b172a977f0f4e4a6f34c266ae98e
Darren Dollery <42651876+darrendollery@users.noreply.github.com>
2021-11-08 21:18:38 +0000

    Add files via upload

---
+++ ex4.4.c
@@ -0,0 +1,21 @@
+/* A program to print the first 10 factorials
+   n! 1....10 */
+
+#include <stdio.h>
+
+int main (void) {
+
+  int n, factorial;
+  int count;
+  for (count = 1; count <= 10; ++count){
+    for (n=1, factorial=1; n <= count; ++n) {
+    factorial = factorial * n;
+    }
+    printf("Factorial %i! is %i\n", count, factorial);
+  }
+
+  return 0;
+}
+    
+    
+    
